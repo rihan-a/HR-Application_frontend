72c29d6429ae336ecf858256acfbb7229e0b2499
rihan-a <a.rihan@live.com>
2025-09-06 13:00:24 +0200

    export loading spinner in index

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -2,5 +2,6 @@ export { Button } from './Button';
 export { Input } from './Input';
 export { Card } from './Card';
 export { Badge } from './Badge';
+export { LoadingSpinner } from './LoadingSpinner';
 export { ToastComponent } from './Toast';
 export { ToastProvider, useToast } from './ToastProvider';
